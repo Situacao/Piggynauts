32cdda5985171f8bc6c972af1ec9860e11ca0ca9
João Martins <joao.martins@blip.pt>
2017-10-23 18:19:36 +0100

    AC01 - AirConsole add up and controller input already connected

--- Assets/Scripts/Controllers/PlayerMatchControllerTEST.cs
+++ Assets/Scripts/Controllers/PlayerMatchControllerTEST.cs
@@ -1,43 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
 
 public class PlayerMatchControllerTEST : MonoBehaviour {
 
     private PlayerActions plAct; 
+	private bool directionUp;
+	private bool directionDown;
+	private bool directionLeft;
+	private bool directionRight;
 
 	void Awake ()
     {
+		directionUp = false;
+		directionDown = false;
+		directionRight = false;
+		directionLeft = false;
+
         plAct = GetComponent<PlayerActions>();
+		AirConsole.instance.onMessage += OnMessage;
 	}
 	
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+		if (directionUp)
         {
-            plAct.VerticalAxisVal = 1f;
+			plAct.VerticalAxisVal = 1f;
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+		else if (directionDown)
         {
-            plAct.VerticalAxisVal = -1f;
+			plAct.VerticalAxisVal = -1f;
         }
         else
         {
             plAct.VerticalAxisVal = 0f;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+		if (directionRight)
         {
-            plAct.HorizontalAxisVal = 1f;
+			plAct.HorizontalAxisVal = 1f;
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+		else if (directionLeft)
         {
-            plAct.HorizontalAxisVal = -1f;
+			plAct.HorizontalAxisVal = -1f;
         }
         else
         {
             plAct.HorizontalAxisVal = 0f;
         }
+			
 
     }
+
+	void OnMessage(int from, JToken data) {
+		AirConsole.instance.Message(from, "FULL");
+
+		string smash = (string) data["data"]["smash"];
+		string key = (string) data["data"]["key"];
+		bool pressed = (bool) data["data"]["pressed"];
+
+
+		if (key == "up" && pressed)
+			directionUp = true;
+		else
+			directionUp = false;
+
+		if (key == "down" && pressed)
+			directionDown = true;
+		else
+			directionDown = false;
+
+		if (key == "left" && pressed)
+			directionLeft = true;
+		else
+			directionLeft = false;
+
+		if (key == "right" && pressed)
+			directionRight = true;
+		else
+			directionRight = false;
+
+		Debug.Log (key + " - " + data); 
+
+
+		if (!string.IsNullOrEmpty(smash))
+			Debug.Log (smash);
+
+	}
+		
 }
