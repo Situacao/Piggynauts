2da1ae1b05daea231a077c300661a600f3a62636
João Vinagre <jpvinagre6@gmail.com>
2018-11-12 11:46:57 +0000

    Created basic instantiation and controller assignment for the CombatTest scene

---
+++ Assets/Scripts/Controllers/ControllerMatch.cs
@@ -0,0 +1,25 @@
+﻿using Rewired;
+using UnityEngine;
+
+public class ControllerMatch : MonoBehaviour
+{
+    private Player pl;
+
+
+    public void Setup(int _plId)
+    {
+        pl = ReInput.players.GetPlayer(_plId);
+
+
+        // Add Events
+        pl.AddInputEventDelegate(OnMovementPress, UpdateLoopType.Update, InputActionEventType.AxisActive, "HorMov");
+    }
+
+    void OnMovementPress(InputActionEventData data)
+    {
+
+        print("Hello");
+
+    }
+
+}
---
+++ Assets/Scripts/TestScene/TestSceneLauncher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TestSceneLauncher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PiggyPrefab;
+
+    void Start()
+    {
+        SetupScene();
+    }
+
+    void SetupScene()
+    {
+        // Instantiate GameObjects
+        GameObject instPrefab = Instantiate(PiggyPrefab);
+        instPrefab.transform.position = Vector3.zero;
+        instPrefab.name = "Piggynaut_P1";
+
+        // Setup Controllers
+        ControllerMatch ctrl = instPrefab.AddComponent<ControllerMatch>();
+        ctrl.Setup(0);
+    }
+
+
+}
